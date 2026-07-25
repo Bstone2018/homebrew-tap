ecd8e3e9ef329a175057a26bb5883c1895eeade3
Robert Bowers <22251974+Bstone2018@users.noreply.github.com>
2026-07-25 07:42:40 -0400

    bstone-finder 1.3

--- Casks/bstone-finder.rb
+++ Casks/bstone-finder.rb
@@ -1,6 +1,6 @@
 cask "bstone-finder" do
-  version "1.2"
-  sha256 "f3d90a3bb4947f8fe91d17607b49170ff6e10ff4c5b7a928581f26351944b383"
+  version "1.3"
+  sha256 "9defcd8ea3c645aba5cd219ad16f21adcd71fc3849691ef0316c8f4427e82f15"
 
   url "https://github.com/Bstone2018/bstone-finder/releases/download/v#{version}/Bstone.Solutions.-.Finder.#{version}.dmg"
   name "Bstone Solutions - Finder"
